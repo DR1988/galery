08a04e78730466a9bf995d3c8046353c20b4383a
ARTGROUP\drakhmanov <Damir.Rakhmanov@artezio.com>
2018-10-15 15:43:09 +0400

    added HtmlWebpackPlugin

--- app/galery.js
+++ app/galery.js
@@ -1,6 +1,6 @@
 import './test.css'
 export default () => {
-  let a = () => 3
+  let a = () => 4
   console.log(22);
   Promise.resolve(console.log(a()))
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -27,6 +29,10 @@ module.exports = {
   },
   devtool: 'cheap-eval-source-map',
   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      hash: true,
+    }),
     new webpack.HotModuleReplacementPlugin(),
   ]
 };
